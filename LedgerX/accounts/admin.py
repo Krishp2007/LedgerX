9ca0bf1e6d59129627c72d6119ae48602e50691e
Krishp2007 <Krishp2136@gmail.com>
2026-01-01 00:28:45 +0530

    All Models Added To Admin Panel

--- LedgerX/accounts/admin.py
+++ LedgerX/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Shop
 
 # Register your models here.
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = (
+        'shop_name',
+        'owner_name',
+        'user',
+        'is_active',
+        'created_at',
+    )
+
+    list_filter = ('is_active',)
+    search_fields = ('shop_name', 'owner_name', 'user__username', 'user__email')
+
+
+admin.site.register(Shop, ShopAdmin)
--- LedgerX/qr/admin.py
+++ LedgerX/qr/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import QRToken
 
-# Register your models here.
+
+class QRTokenAdmin(admin.ModelAdmin):
+    list_display = (
+        'customer',
+        'secure_token',
+        'is_active',
+        'created_at',
+    )
+
+    list_filter = ('is_active',)
+
+
+admin.site.register(QRToken, QRTokenAdmin)
