59dff1b8f5b537c206d5f50771666d78459156a8
Krishp2007 <Krishp2136@gmail.com>
2026-01-29 22:01:14 +0530

    Double Warning on Login solved & email/welcome Url

---
+++ LedgerX/qr/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<str:secure_token>/', views.customer_ledger_qr, name='customer_ledger_qr'),
+
+    path('image/<int:customer_id>/', views.generate_qr_image, name='qr_image'),
+
+    path('<str:secure_token>/transaction/<int:transaction_id>/',
+    views.qr_transaction_detail,
+    name='qr_transaction_detail'
+    ),
+
+]
---
+++ LedgerX/reports/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.reports_home, name='reports_home'),
+    path('sales/', views.sales_report, name='sales_report'),
+    path('products/', views.product_report, name='product_report'),
+    path('customers/', views.customer_report, name='customer_report'),
+]
+
